763629b76d9b86074958901e9d6b223113098333
Albin Ekblom <albinekblom@gmail.com>
2020-07-26 16:22:16 +0200

    Initial Commit

---
+++ HostButler/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HostButler
+//
+//  Created by Albin Ekblom on 2020-07-26.
+//  Copyright © 2020 Albin Ekblom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
