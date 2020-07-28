3d3740baf42663b3af69f764579f91ea301fdcc5
Albin Ekblom <albinekblom@gmail.com>
2020-07-28 11:55:14 +0200

    Cleanup row view

--- PortButler/ContentView.swift
+++ PortButler/ContentView.swift
@@ -10,13 +10,16 @@ import SwiftUI
 
 struct ContentView: View {
      @ObservedObject var ports = ObservablePorts()
+    
   
     var body: some View {
-//        VStack {
-            PortsListView(ports: self.ports.ports)
-//        }
-//
-//        .frame(minWidth: 200, idealWidth: 320, maxWidth: 600, minHeight: 100, idealHeight: 200, maxHeight: 800, alignment: .top)
+        Group {
+            if self.ports.ports.count > 0 {
+                PortsListView(ports: self.ports.ports)
+            } else {
+                HStack(alignment: .center){Text("No ports open")}.frame(maxHeight: .infinity)
+            }
+        }.frame(minWidth: 200, idealWidth: 320, maxWidth: 600, minHeight: 100, idealHeight: 200, maxHeight: 800, alignment: .top)
     }
     
     public func scan () {
--- PortButler/Views/PortsListView.swift
+++ PortButler/Views/PortsListView.swift
@@ -14,7 +14,6 @@ import SwiftUI
 
 
 struct BrowserView: View {
-
     private let browser = WebBrowserView()
 
     var body: some View {
@@ -29,51 +28,63 @@ struct BrowserView: View {
     }
 }
 
-struct PortRowView: View {
-    var port: Port
-    @State var isHovered: Bool = false
+struct RowView<Child>: View where Child: View {
+    var port: () -> Child
+    var pid: () -> Child
+    var title: () -> Child
+    var action: AnyView
 
-    
     var body: some View {
-        VStack{
+        VStack {
             GeometryReader { geometry in
                 HStack {
-                    HStack{
-                        Text(String(self.port.port)).font(.system(.caption, design: .monospaced))
-                        Spacer()
-                    }.frame(width: geometry.size.width / 4)
-                        HStack{
-                            Text(String(self.port.netstat?.pid ?? "")).font(.system(.caption, design: .monospaced))
-                            Spacer()
-                        }.frame(width: geometry.size.width / 4)
-                    HStack{
-                        Spacer()
-                    }.frame(width: geometry.size.width / 4)
-                    //HStack{Image(nsImage: NSImage(imageLiteralResourceName: NSImage.followLinkFreestandingTemplateName)).onTapGesture(perform: self.openUrl)}.frame(width: geometry.size.width / 4, height: 50)
-                    //Button(action: self.openUrl){Text("Open"}
-                    HStack{
-                        Spacer()
-                        Button(action: self.openUrl){
-                            Image(nsImage: NSImage(imageLiteralResourceName: NSImage.followLinkFreestandingTemplateName)).opacity(0.5)
-                        }
-                            .toolTip("Open URL")
-                            .buttonStyle(PlainButtonStyle())
-                            .onHover(perform: { hovered in
-                              self.isHovered = hovered
-                              if hovered {
-                                NSCursor.pointingHand.push()
-                              } else {
-                                NSCursor.pop()
-                              }
-                            })
-                            .onTapGesture(perform: self.openUrl)
-                        
-                    }.frame(width: geometry.size.width / 4)
+                    HStack{Group(content: self.port);Spacer()}.frame(width: geometry.size.width / 4)
+                    HStack{Group(content: self.pid);Spacer()}.frame(width: geometry.size.width / 4)
+                    HStack{Group(content: self.title);Spacer()}.frame(width: geometry.size.width / 4)
+                    HStack{Spacer();AnyView(self.action);}.frame(width: geometry.size.width / 4)
                 }
             }
         }
-        //.onTapGesture(perform: self.openUrl)
     }
+}
+
+struct OpenPageButton: View {
+    var title: String
+    var action: () -> Void
+    @State var isHovered: Bool = false
+    
+
+
+    var body: some View {
+        Button(action: self.action) {
+            Image(nsImage: NSImage(imageLiteralResourceName: NSImage.followLinkFreestandingTemplateName)).opacity(self.isHovered ? 1 : 0.5)
+            }
+            .toolTip(self.title)
+            .buttonStyle(PlainButtonStyle())
+            .onHover(perform: { hovered in
+              self.isHovered = hovered
+              if hovered {
+                NSCursor.pointingHand.push()
+              } else {
+                NSCursor.pop()
+              }
+            })
+          .onTapGesture(perform: self.action)
+    }
+}
+
+struct PortRowView: View {
+    var port: Port
+    
+    var body: some View {
+        RowView(
+            port: {Text(String(self.port.port)).font(.system(.caption, design: .monospaced))},
+            pid: {Text(String(self.port.netstat?.pid ?? "")).font(.system(.caption, design: .monospaced))},
+            title: {Text("N/a")},
+            action: AnyView(OpenPageButton(title: "Open URL", action: self.openUrl))
+        )
+    }
+    
     
     func openUrl() {
         let url = URL(string: "http://localhost:" + String(self.port.port))!
@@ -87,6 +98,9 @@ struct PortsListView: View {
     
     var body: some View {
         Group{
+            List([0], id: \.self) { i in
+                RowView(port: {Text("Port")}, pid: {Text("Pid")}, title: {Text("Title")}, action: AnyView(Text("Action"))).frame(height: 40)
+                }.listStyle(SidebarListStyle()).frame(maxHeight: 30)
             List(self.ports, id: \.self) { port in
               PortRowView(port: port).frame(height: 40)
             }.listStyle(SidebarListStyle())
